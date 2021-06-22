04a6138230f3702a3087dd8843b90b849fbbce04
quyctd <quy.dc98@gmail.com>
2021-06-22 10:46:59 +0700

    Setup dev mongodb

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
+
+mongoose.connect("mongodb://localhost:27017/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
 
 app.set("view engine", "ejs")
 app.get("/", (req, res) => {
   res.render("index")
+})
+
+app.post("/shortUrls", (req, res) => {
+  //Connect DB - shortening
 })
 
 app.listen(process.env.PORT || 3000, () => {
