105a15cfa1d09773c8f31e8b7f0f96fd18b9b278
quyctd <quy.dc98@gmail.com>
2021-06-22 10:28:57 +0700

    Basic styling

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
 
-app.listen(3000, () => {
+app.set("view engine", "ejs")
+app.get("/", (req, res) => {
+  res.render("index")
+})
+
+app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port 3000")
 })
