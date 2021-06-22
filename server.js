2f0fc0ab58eaeb9f406c1d3420fbba93f53aba4b
quyctd <quy.dc98@gmail.com>
2021-06-22 10:56:21 +0700

    Define schema

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const ShortUrl = require("./models/shortUrl")
 const app = express()
 
 mongoose.connect("mongodb://localhost:27017/urlShortener", {
@@ -8,13 +9,15 @@ mongoose.connect("mongodb://localhost:27017/urlShortener", {
   useCreateIndex: true,
 })
 
+app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 app.get("/", (req, res) => {
   res.render("index")
 })
 
-app.post("/shortUrls", (req, res) => {
-  //Connect DB - shortening
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+  res.redirect("/")
 })
 
 app.listen(process.env.PORT || 3000, () => {
